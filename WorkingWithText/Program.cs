3908170a3ee9072407db9615dfb55a87148b6c8c
Manuel Phetla <mnockz@gmail.com>
2020-06-08 19:26:39 +0200

    Exercise number 03

--- WorkingWithText/Program.cs
+++ WorkingWithText/Program.cs
@@ -10,14 +10,20 @@ namespace WorkingWithText
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter few numbers separated by a hyphen eg (1-2-3-4)");
-            string numbers = Console.ReadLine();
+            //Console.WriteLine("Please enter few numbers separated by a hyphen eg (1-2-3-4)");
+            //string numbers = Console.ReadLine();
 
             //Exercise 01
             //Console.WriteLine(TextExercise01.ConsecutiveNumbers(name));
 
             //Exercise 02
-            Console.WriteLine(TextExercise02.Duplicate(numbers));
+            //Console.WriteLine(TextExercise02.Duplicate(numbers));
+
+            //Exercise 03
+            Console.WriteLine("Please enter a time value in the 24-hour time format (e.g. 19:00)");
+            string timeValue = Console.ReadLine();
+            Console.WriteLine(TextExercise03.TimeValue(timeValue));
+
 
         }
     }
---
+++ WorkingWithText/TextExercise03.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithText
+{
+    public class TextExercise03
+    {
+        /*
+         3- Write a program and ask the user to enter a time value in the 24-hour time format (e.g. 19:00). 
+         A valid time should be between 00:00 and 23:59. If the time is valid, display "Ok"; 
+         otherwise, display "Invalid Time". If the user doesn't provide any values, consider it as invalid time.
+         */
+
+        public static string TimeValue(string timeValue)
+        {
+
+            var index = timeValue.Split(':');
+            var results = "Invalid Time";
+
+            if (Convert.ToInt32(index[0]) >= 0 && Convert.ToInt32(index[0]) <= 23)
+            {
+                if (Convert.ToInt32(index[1]) >= 0 && Convert.ToInt32(index[1]) <= 59)
+                {
+                    results = "Ok";
+                    return results;
+                }                
+            }
+
+            return results;
+        }
+
+    }
+}

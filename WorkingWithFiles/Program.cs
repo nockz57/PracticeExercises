f765dfe88019891163f0f4a8fe634f267c450201
Manuel Phetla <mnockz@gmail.com>
2020-06-13 19:36:58 +0200

    Started working with files exercise

---
+++ WorkingWithFiles/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing 1 2, 1 2");
+
+            //use full file path (e.g c:\users\............)
+            var path = @"C:\Users\mphet\source\home\PracticeExercises\DestinationFiles\Lorem.txt";
+            File.Copy(@"C:\Users\mphet\source\home\PracticeExercises\SourceFiles\Lorem.txt", path, true);
+
+            if (File.Exists(path))
+            {
+                Console.WriteLine("The file exist");
+            }
+
+            var builder = new StringBuilder();
+            builder.Append('-', 115);
+            Console.WriteLine(builder);
+            Console.WriteLine(File.ReadAllText(path));
+            Console.ReadLine();
+
+        }
+    }
+}
